e6fe3455b139f63dda4ae653f51e545bf243f792
silvaayla <156119647+silvaayla@users.noreply.github.com>
2024-01-30 17:35:01 -0300

    revisão do código

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
+import {db, firestore} from '../banco-de-dados/firebase';
 
 const app = express();
 
 
-
-app.listen(3000, function () {
-    console.log("Serviço rodando em http:// localhost:3000")
+app.listen(3000, function(){
+    console.log("Serviço rodando em http://localhost:3000")
 });
