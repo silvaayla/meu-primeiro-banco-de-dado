42a08eaf7468f6ead0278a5b175a64591a99fcb0
silvaayla <156119647+silvaayla@users.noreply.github.com>
2024-02-01 17:33:51 -0300

    primeiro usuário criado

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,32 @@
 import express from "express";
-import {db, firestore} from '../banco-de-dados/firebase';
+import { db, firestore } from '../banco-de-dados/firebase';
 
 const app = express();
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Bem vindo a minha primeira API')
 })
 
-app.post('/usuario', async (req, res) =>{
-    const usuario = req.body.nome
+app.post('/usuario', async (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email
+    const telefone = req.body.telefone
 
 
     try {
-        const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'),{
-nome: nome,
- })
-    res.send(docRef.id)
-    }catch (e) ()
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'), {
+            nome: nome,
+            email: email,
+            telefone: telefone
+        })
+        res.send("Usuario adicionado com sucesso:" + docRef.id)
+    } catch (e) {
+        console.log(e)
+
+        res.status(500).send(e)
+    }
 });
 
 
